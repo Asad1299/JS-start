8bb416e063c57978f654784d50080ec389761449
Asad1299 <ulhaqasad4@gmail.com>
2026-06-02 14:24:35 +0000

    container config done

---
+++ 01_Basics/ConversionOperat.js
@@ -0,0 +1,29 @@
+/*let score= null;
+console.log(typeof score);
+console.log(score);
+let AnotherNumber = Number(score);
+console.log(typeof AnotherNumber);
+console.log(AnotherNumber); */
+
+//All the mentioned datatypes, when converted into number, give (and their genuine datatypes are also mentioned):
+// "33" => 33
+// "33ab" => NaN (not a number, but its type is number😂)
+// true => 1
+// false => 0
+// null => 0 (datatype is object)
+//undefined => NaN (data type is also "undefined")
+
+/*let IsLoggedIn = "Asad";
+let BooleanLoggedin= Boolean(IsLoggedIn);
+console.log(BooleanLoggedin);
+console.log(typeof BooleanLoggedin); */
+
+// Number converted to Boolean:
+// 1 => true, 0 => false (datatype became boolean)
+// "" => false
+// "Asad" => true
+
+let Somenumber = 33;
+let stringnumber = String(Somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);
